18fa38899241be17f107a640805ee2923baeca6e
Fickry Iman <fickry.iman@gmail.com>
2023-05-03 08:48:48 +0700

    manage application state to one source (Todos context)

--- src/components/TodosList.jsx
+++ src/components/TodosList.jsx
@@ -1,16 +1,14 @@
 import TodoItem from '@/components/TodoItem';
+import { useTodosContext } from '@/context/TodosContext';
 
-const TodosList = ({ todosProps, handleChange, deleteTodo, setUpdate }) => {
+const TodosList = () => {
+  
+  const { todos } = useTodosContext();
+
   return (
     <ul>
-      {todosProps.map((todo) => (
-        <TodoItem 
-          key={todo.id} 
-          itemProp={todo} 
-          handleChange={handleChange} 
-          deleteTodo={deleteTodo}
-          setUpdate={setUpdate}
-        />
+      {todos.map((todo) => (
+        <TodoItem key={todo.id} itemProp={todo} />
       ))}
     </ul>
   );
